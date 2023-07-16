98e95f55fc819ba109baffca4c057d429537de93
Ryan Alexander Acosta <67177782+rAlexander89@users.noreply.github.com>
2023-07-15 22:09:37 -0700

    Update friends_controller.rb

--- app/controllers/api/v1/friends_controller.rb
+++ app/controllers/api/v1/friends_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::FriendsController < ApplicationController
   require 'debug'
+  
   def index
     friends = Friend.all 
     if friends
@@ -8,7 +9,7 @@ class Api::V1::FriendsController < ApplicationController
       render json: friends.errors, status: :bad_request
     end
   end
-  # create a new friend Post request
+  
   def create
     @friend = Friend.new(friend_params)
     
@@ -18,7 +19,7 @@ class Api::V1::FriendsController < ApplicationController
       render json: @friend.errors, status: :unprocessable_entity
     end
   end
-  # Show a specific friend GET request
+  
   def show
     friend = Friend.find(params[:id])
 
@@ -28,8 +29,6 @@ class Api::V1::FriendsController < ApplicationController
       render json: {message: "Friend could not be found"}, status: :bad_request
     end
   end
-
-  # Delete a specific friend DELETE request
   def destroy
     friend = Friend.find(params[:id])
 
@@ -40,7 +39,6 @@ class Api::V1::FriendsController < ApplicationController
     end
   end
 
-  # Update details for a specific friend. PATCH request
   def update
     friend = Friend.find(params[:id])
 
@@ -55,12 +53,45 @@ class Api::V1::FriendsController < ApplicationController
 
   def friend_params
     params.require(:friend).permit(
-      { name: [:name_title, :name_first, :name_middle, :name_last, :name_suffix] },
-      :dob,
-      :phone,
-      :twitter_handle,
-      :email,
-      { address: [:delivery_line_1, :last_line, :delivery_point_bar_code, :street_number, :street_name, :street_suffix, :city, :county, :county_FIPS, :state_abbreviation, :country, :country_code, :postal_code, :zip_plus_4_extension, :zip_type, :delivery_point, :delivery_point_check_digit, :carrier_route, :record_type, :latitude, :longitude] },
+      { 
+        name: 
+        [
+          :name_title, 
+          :name_first, 
+          :name_middle, 
+          :name_last, 
+          :name_suffix
+        ] 
+    },
+    :dob,
+    :phone,
+    :twitter_handle,
+    :email,
+    { 
+      address: 
+      [
+        :delivery_line_1, 
+        :last_line, 
+        :delivery_point_bar_code, 
+        :street_number, 
+        :street_name, 
+        :street_suffix, 
+        :city, 
+        :county, 
+        :county_FIPS, 
+        :state_abbreviation, 
+        :country, :country_code, 
+        :postal_code, 
+        :zip_plus_4_extension, 
+        :zip_type, 
+        :delivery_point, 
+        :delivery_point_check_digit, 
+        :carrier_route, 
+        :record_type, 
+        :latitude, 
+        :longitude
+      ]
+    },
       :available_to_party
     )
   end
